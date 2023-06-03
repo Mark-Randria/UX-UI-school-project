879b768c526684dc19b6f47fe822e12c9e76f572
Mark-Randria <randrianjafymark2.0@gmail.com>
2023-06-03 23:56:52 +0300

    Adding colors values of the light mode

---
+++ my-app/src/core/theme/light.js
@@ -0,0 +1,23 @@
+import {
+  tealA,
+  greenA,
+  blueA,
+  indigoA,
+  amberA,
+  redA,
+  gray,
+} from "@radix-ui/colors";
+
+const theme = {
+  colors: {
+    ...tealA,
+    ...greenA,
+    ...blueA,
+    ...indigoA,
+    ...amberA,
+    ...redA,
+    ...gray,
+  },
+};
+
+export default theme;

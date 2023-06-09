3720a60ccfa566ba8d0f2e1dcf5ad44b52e00192
Mark-Randria <randrianjafymark2.0@gmail.com>
2023-06-09 17:28:49 +0300

    Fix button size on mobile

--- my-app/src/components/buttons/button.style.js
+++ my-app/src/components/buttons/button.style.js
@@ -11,7 +11,7 @@ export const StyledButton = styled.button`
   font-weight: 600;
   max-width: 162px;
   width: ${(props) => props.$width};
-  min-width: 80px;
+  min-width: 78px;
   height: ${(props) => props.$height};
   ${(props) => {
     switch (props.$mode) {
